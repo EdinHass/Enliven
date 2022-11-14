9ee2235bcaeb343497f935602bd1f0d165a869b5
ayla <frkic.ajla@gmail.com>
2022-11-14 22:34:07 +0100

    V1.0 Navbar

---
+++ app/src/main/java/com/example/enliven/SrecaActivity.java
@@ -0,0 +1,14 @@
+package com.example.enliven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SrecaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sreca);
+    }
+}
