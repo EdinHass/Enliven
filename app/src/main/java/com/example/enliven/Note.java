03980e74e4335e93229611117aecd6dbcbe9aed6
ayla <frkic.ajla@gmail.com>
2022-11-16 22:36:20 +0100

    V1.0 Navbar

---
+++ app/src/main/java/com/example/enliven/Note.java
@@ -0,0 +1,6 @@
+package com.example.enliven;
+
+public class Note {
+    String title;
+    String description;
+}
--- app/src/main/java/com/example/enliven/diaryactivity.java
+++ app/src/main/java/com/example/enliven/diaryactivity.java
@@ -3,10 +3,25 @@ package com.example.enliven;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class diaryactivity extends AppCompatActivity {
+    ListView notesListView;
+    TextView emptyText;
 
+    static List<String> notes;
+    static ArrayAdapter adapter;
+
+    SharedPreferences sharedpref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +29,20 @@ public class diaryactivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Dnevnik");
+
+        sharedpref = this.getSharedPreferences("com.example.enliven", Context.MODE_PRIVATE);
+
+        notesListView = findViewById(R.id.notes_ListView);
+        emptyText = findViewById(R.id.emptyText);
+
+        notes = new ArrayList<>();
+
+        HashSet<String> noteSet = (HashSet<String>) sharedpref.getStringSet("notes", null);
+
+        if(noteSet.isEmpty() )
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.custom_notes_row, R.id.textView5, notes);
+        notesListView.setAdapter(adapter);
+
     }
     @Override
     public boolean onSupportNavigateUp() {
