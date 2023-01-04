6d2cebaed39df95b05c8bfa3fa4eb59e02f500d3
EdinHass <edinhasanovic03@gmail.com>
2023-01-04 13:38:45 +0100

    V1.6.2 Sound Player Timer

--- app/src/main/java/com/example/enliven/SoundsPlayerActivity.java
+++ app/src/main/java/com/example/enliven/SoundsPlayerActivity.java
@@ -1,18 +1,28 @@
 package com.example.enliven;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.fragment.app.DialogFragment;
 import androidx.palette.graphics.Palette;
 
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Picture;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -20,22 +30,29 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.slider.LabelFormatter;
+import com.google.android.material.slider.Slider;
 
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import pl.droidsonroids.gif.GifImageView;
 
-public class SoundsPlayerActivity extends AppCompatActivity {
+public class SoundsPlayerActivity extends AppCompatActivity implements TimerDialogFragment.NoticeDialogListener{
 
     String SoundName;
     int SoundData, PictureData;
     TextView Title;
     MediaPlayer mediaPlayer;
-    ImageView startButton, soundImage;
+    ImageView startButton, soundImage, volumeIcon;
     GifImageView gif;
     RelativeLayout back;
+    Slider slider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +64,25 @@ public class SoundsPlayerActivity extends AppCompatActivity {
         back = findViewById(R.id.backgroundSound);
         Title = findViewById(R.id.soundName);
         gif = findViewById(R.id.gif);
+        slider = findViewById(R.id.slider);
+        volumeIcon = findViewById(R.id.volumeIcon);
+        startButton = findViewById(R.id.startButton);
+        Drawable soundIcon = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.ic_baseline_volume_up_24);
+        Drawable playIcon = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.ic_baseline_play_arrow_24);
+        Drawable pauseIcon = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.ic_baseline_pause_24);
 
+        slider.setValue(0.5f);
+        slider.setLabelFormatter(new LabelFormatter() {
+            @NonNull
+            @Override
+            public String getFormattedValue(float value) {
+                return Math.round(value*100) + "%";
+            }
+        });
+
+        Drawable soundIconWrapped = DrawableCompat.wrap(soundIcon);
+        Drawable pauseIconWrapped = DrawableCompat.wrap(pauseIcon);
+        Drawable playIconWrapped = DrawableCompat.wrap(playIcon);
         Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), PictureData);
         Palette palette = Palette.from(bitmap).generate();
         Palette.Swatch vibrant = palette.getDarkVibrantSwatch();
@@ -58,7 +93,27 @@ public class SoundsPlayerActivity extends AppCompatActivity {
             gd.setGradientType(GradientDrawable.LINEAR_GRADIENT);
             gd.setGradientRadius(1.0f);
             back.setBackground(gd);
+            slider.setTrackActiveTintList(ColorStateList.valueOf(palette.getVibrantSwatch().getRgb()));
+            slider.setTrackInactiveTintList(ColorStateList.valueOf(vibrant.getRgb()));
+            slider.setThumbTintList(ColorStateList.valueOf(palette.getVibrantSwatch().getRgb()));
+            DrawableCompat.setTint(soundIconWrapped, palette.getVibrantSwatch().getRgb());
+            volumeIcon.setImageDrawable(soundIconWrapped);
+            DrawableCompat.setTint(pauseIconWrapped, vibrant.getRgb());
+            DrawableCompat.setTint(playIconWrapped, vibrant.getRgb());
+            startButton.setImageDrawable(playIconWrapped);
+
+
+
         }
+
+        slider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.setVolume(slider.getValue(), slider.getValue());
+                }
+            }
+        });
 
 
 
@@ -68,7 +123,7 @@ public class SoundsPlayerActivity extends AppCompatActivity {
         actionBar.setTitle(SoundName);
 
 
-        startButton = findViewById(R.id.startButton);
+
         Title.setText(SoundName);
 
         soundImage = (ImageView)findViewById(R.id.soundImage);
@@ -86,14 +141,16 @@ public class SoundsPlayerActivity extends AppCompatActivity {
                 Animation animFadeout = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
                 if(mediaPlayer.isPlaying()){
                     mediaPlayer.pause();
-                    startButton.setImageResource(R.drawable.ic_baseline_play_arrow_24);
+                    startButton.setImageDrawable(playIconWrapped);
                     startButton.startAnimation(animFadein);
                     gif.startAnimation(animFadeout);
                     gif.setVisibility(View.GONE);
 
                 }else{
+                    mediaPlayer.seekTo(0);
+                    mediaPlayer.setVolume(slider.getValue(), slider.getValue());
                     mediaPlayer.start();
-                    startButton.setImageResource(R.drawable.ic_baseline_pause_24);
+                    startButton.setImageDrawable(pauseIconWrapped);
                     startButton.startAnimation(animFadein);
                     gif.startAnimation(animFadein);
                     gif.setVisibility(View.VISIBLE);
@@ -108,6 +165,23 @@ public class SoundsPlayerActivity extends AppCompatActivity {
         finish();
         return true;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sound_player_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.timer){
+            DialogFragment newFragment = new TimerDialogFragment();
+            newFragment.show(getSupportFragmentManager(), "Timer");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private float interpolate(float a, float b, float proportion) {
         return (a + ((b - a) * proportion));
@@ -144,5 +218,50 @@ public class SoundsPlayerActivity extends AppCompatActivity {
                 Math.min(r,255),
                 Math.min(g,255),
                 Math.min(b,255));
+    }
+
+    @Override
+    public void onDialogPositiveClick(TimerDialogFragment dialog) {
+        Timer myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                TimerMethod();
+                myTimer.cancel();
+                myTimer.purge();
+            }
+
+        }, Math.round(dialog.slider.getValue()*100)*2*60*1000, 3000);
+
+        Toast.makeText(getApplicationContext(), "Timer postavljen za: " + getFormattedValue(dialog.slider.getValue()), Toast.LENGTH_SHORT).show();
+    }
+
+    private void TimerMethod(){
+        this.runOnUiThread(Timer_Tick);
+    }
+
+    private Runnable Timer_Tick = new Runnable() {
+        public void run() {
+            if(mediaPlayer.isPlaying()) {
+                startButton.callOnClick();
+                Toast.makeText(getApplicationContext(), "Timer: Aktiviran!", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "Timer: Zvuk je vec pauziran!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    public void onDialogNegativeClick(TimerDialogFragment dialog) {
+
+    }
+
+    public String getFormattedValue(float value) {
+        int val = Math.round(value*100);
+        int minutes = val*2;
+        int hours = minutes/60;
+        minutes = minutes % 60;
+        String res = hours + "h " + minutes + "m";
+        return res;
     }
 }
---
+++ app/src/main/java/com/example/enliven/TimerDialogFragment.java
@@ -0,0 +1,87 @@
+package com.example.enliven;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.slider.LabelFormatter;
+import com.google.android.material.slider.Slider;
+
+public class TimerDialogFragment extends DialogFragment {
+
+    public interface NoticeDialogListener {
+        public void onDialogPositiveClick(TimerDialogFragment dialog);
+        public void onDialogNegativeClick(TimerDialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener listener;
+    Slider slider;
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View layoutDialog = inflater.inflate(R.layout.timer_dialog, null);
+        slider = layoutDialog.findViewById(R.id.sliderTimer);
+
+
+        slider.setLabelFormatter(new LabelFormatter() {
+            @NonNull
+            @Override
+            public String getFormattedValue(float value) {
+                int val = Math.round(value*100);
+                int minutes = val*2;
+                int hours = minutes/60;
+                minutes = minutes % 60;
+                String res = hours + "h " + minutes + "m";
+                return res;
+            }
+        });
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(layoutDialog)
+                // Add action buttons
+                .setPositiveButton("SET", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDialogPositiveClick(TimerDialogFragment.this);
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDialogNegativeClick(TimerDialogFragment.this);
+                        TimerDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+}
