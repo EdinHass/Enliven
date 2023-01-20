50351998b553940ecd4519ed1b0487656b5f6578
ayla <frkic.ajla@gmail.com>
2023-01-20 19:29:57 +0100

    V1.0 Navbar

---
+++ app/src/main/java/com/example/enliven/vjezba_disanja_za_san.java
@@ -0,0 +1,64 @@
+package com.example.enliven;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link vjezba_disanja_za_san#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class vjezba_disanja_za_san extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public vjezba_disanja_za_san() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment vjezba_disanja_za_san.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static vjezba_disanja_za_san newInstance(String param1, String param2) {
+        vjezba_disanja_za_san fragment = new vjezba_disanja_za_san();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vjezba_disanja_za_san, container, false);
+    }
+}
